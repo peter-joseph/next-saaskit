2f986ffcd6083d6a1e6b9d039d3e65e0ca24eac4
Ivy Olamit <ivyr.ca@gmail.com>
2026-02-27 15:29:34 -0800

    added configuration for test coverage

--- packages/uxcomponents/jest.config.js
+++ packages/uxcomponents/jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageThreshold: {
+    "branches": 50,
+    "functions": 100,
+    "lines": 100,
+    "statements": 100
+  }
 };
